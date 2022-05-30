f1e56c70a5b2f3e54c5cddd0b7803749465b754f
postmodernist1488 <elitic.pantheism@gmail.com>
2022-05-30 15:26:50 +0300

    started reading KnR

---
+++ void.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int foo(void * a, void * b){
+    return *(int*) a + *(int *) b;
+}
+
+int main(void) {
+    int a=10, b = 5;
+    int *p_a = &a;
+    int *p_b = &b;
+    printf("%d\n", foo(p_a, p_b));
+    return 0;
+}
