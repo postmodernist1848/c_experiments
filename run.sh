8c8fd166a80190268676a13699c1576edfdad072
postmodernist1488 <elitic.pantheism@gmail.com>
2022-06-08 13:53:45 +0300

    fix fatal buuuug

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 # A simple script to compile and run a single C file.
-if [ $# -lt 1 ]; then echo "Error: not enough arguments." > /dev/stderr; exit 1
-elif [ $# - gt 1 ]; then echo "Error: too many arguments." > /dev/stderr; exit 1
+if [ $# -lt 1 ]; then 
+echo "Error: not enough arguments." > /dev/stderr
+exit 1
+elif [ $# -gt 1 ]; then
+echo "Error: too many arguments." > /dev/stderr
+exit 1
 else
+    f=$1
     CFLAGS="-Wall -Wextra"
     exe="${f%.*}"
-    if gcc $f $CFLAGS -o $exe && ./$exe; rm $exe 
+    if gcc $f $CFLAGS -o $exe && ./$exe; then 
+    rm $exe 
+fi
 fi
