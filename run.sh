aff1033f094b3fe5f9abaabdb87720dbe428e7df
postmodernist1488 <elitic.pantheism@gmail.com>
2022-07-04 12:43:17 +0300

    run.sh now returns correct exitcode

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# A simple script to compile and run a single C file.
+# A simple script to compile and run a single C or Python file.
 if [ $# -lt 1 ]; then 
 echo "Error: not enough arguments." > /dev/stderr
 exit 1
@@ -7,10 +7,32 @@ elif [ $# -gt 1 ]; then
 echo "Error: too many arguments." > /dev/stderr
 exit 1
 else
+    filename=$(basename -- "$1")
+    extension="${filename##*.}"
     f=$1
-    CFLAGS="-Wall -Wextra"
-    exe="${f%.*}"
-    if gcc $f $CFLAGS -o $exe && ./$exe; then 
-    rm $exe 
-fi
+    case $extension in
+        py)
+            python3 $f
+            exit $?
+            ;;
+        c)
+            CFLAGS="-Wall -Wextra -lm"
+            exe="${f%.*}"
+            if gcc $f $CFLAGS -o $exe; then 
+                ./$exe
+                exitcode=$?
+                rm $exe 
+                exit $exitcode
+            else 
+                exit $?
+            fi
+            ;;
+        sh)
+            ./$f
+            exit $?
+            ;;
+        *)
+            echo "Unknown filetype \`$extension\`" > /dev/stderr
+            ;;
+    esac
 fi
